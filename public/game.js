291000e3412e548b2e8242bc9a1c4f65a277f901
Jonathan Giroux <bloutiouf@gmail.com>
2015-04-04 01:34:51 +0200

    Second part

--- public/game.js
+++ public/game.js
@@ -257,8 +257,8 @@ function game() {
 								audioStartTime = audioCtx.currentTime;
 								isPlaying = true;
 								resetNotes();
-								currentStage = 6; // TODO
-								currentStageTarget = 6;
+								currentStage = 8; // TODO
+								currentStageTarget = 8;
 
 								var gainNode = audioCtx.createGain();
 								var sourceNode = audioCtx.createBufferSource();
@@ -277,7 +277,7 @@ function game() {
 									endTime: endTime
 								};
 
-								audioStartTime -= fromMusicalTime(79); // TODO
+								audioStartTime -= fromMusicalTime(112); // TODO
 
 								pushNextSource();
 							}, function() {
